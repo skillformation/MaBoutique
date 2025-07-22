d9119a298ece46aec34d83838bc24e6bda91d2e8
Blackamdev <outback.mimes_0d@icloud.com>
2025-07-22 09:56:06 -0400

    modification de code de la fonction pour protéger l'accés à fillaments

--- BoutiqueApp/app/Models/User.php
+++ BoutiqueApp/app/Models/User.php
@@ -3,11 +3,13 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Filament\Panel;
+use Illuminate\Notifications\Notifiable;
+use Filament\Models\Contracts\FilamentUser;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
 
-class User extends Authenticatable
+class User extends Authenticatable implements FilamentUser
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
@@ -48,8 +50,11 @@ class User extends Authenticatable
     }
 
     //Protéger l'accés à fillaments 
-    public function canAccessFilament(): bool
+    public function canAccessPanel(Panel $panel): bool
     {
         return $this->role === 'admin';
     }
+   
+    
+    
 }
